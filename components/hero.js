e2059c995cd3740e802ed41c155db2314360be4d
kinh17631-star <kinh17631@gmail.com>
2026-04-09 11:51:17 +0530

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -90,7 +90,7 @@ const container = {
   position: "relative",
   width: "100%",
   height: "100vh", // Full screen for cinematic feel
-  backgroundImage: "url('/IMG_4532.jpeg')",
+  backgroundImage: "url('/IMG_4538.webp')",
   backgroundSize: "cover",
   backgroundPosition: "center",
   display: "flex",
